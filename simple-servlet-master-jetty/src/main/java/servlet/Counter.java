a63e22e05d2153ee36d06b9ce7883eed90924e3c
eneascarneiro <jose.manuel.aroca@hotmail.com>
2022-06-22 16:48:06 +0200

    jetty

---
+++ simple-servlet-master-jetty/src/main/java/servlet/Counter.java
@@ -0,0 +1,54 @@
+package servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+
+public class Counter extends HttpServlet {
+    int accesses = 0;
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+         throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        //Leer el parámetro acc de request
+        //Convertirlo a entero
+        //Implementar la logica para controlar la entrada
+        //“” (si no hay valor)
+        //null (si no existe).
+        //El valor en caso de haber sido establecido.
+        //out.print("Este es el request");
+        //out.print(request);
+        int entrada_acc = -2;
+        if (request.getParameter("acc") == null || request.getParameter("acc") =="" )
+        {
+            entrada_acc = 0;
+            out.print("   <!DOCTYPE html>   " +
+                    "<html>  " +
+                    "<body> " +
+                    "<p>Number of times this servlet has been accessed: desconocido "+
+                    " </p> " +
+                    " </body> " +
+                    " </html>");
+        } else
+        {
+            entrada_acc = Integer.parseInt(request.getParameter("acc")) + 27;
+            out.print("   <!DOCTYPE html>   " +
+                    "<html>  " +
+                    "<body> " +
+                    "<p>Number of times this servlet has been accessed:  "  + entrada_acc   +
+                    " </p> " +
+                    " </body> " +
+                    " </html>");
+        }
+
+
+
+
+    }
+} 
---
+++ simple-servlet-master-jetty/src/main/java/utils/token.java
@@ -0,0 +1,15 @@
+package utils;
+
+import java.util.Random;
+
+public class token {
+    public static String generateRandomPassword(int len) {
+        String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijk"
+                +"lmnopqrstuvwxyz!@#$%&";
+        Random rnd = new Random();
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++)
+            sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        return sb.toString();
+    }
+}
